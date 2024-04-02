724945db12b378fef23dadbcf2be94cf81ed991c
Nicopobletee <nicopobletel@gmail.com>
2024-04-02 13:25:27 -0300

    Update Model.jsx

--- vite-project/src/components/Model.jsx
+++ vite-project/src/components/Model.jsx
@@ -11,6 +11,10 @@ export function Model({ id, color, scale, onClick, isSelected, ...props }) {
         <mesh geometry={nodes['Material0-material'].geometry} material={materials['Material0-material']} position={[0.003, -0.142, 0.03]} rotation={[-0.007, -0.03, -0.156]} scale={[1.273, 1.169, 0.783]}>
           {isSelected && (
               <meshLambertMaterial opacity={isSelected ? "0.9" : "1"} transparent color="#ccc"/>
+          )
+          }
+          {id !== 1 && (
+            <meshPhysicalMaterial />
           )}
         </mesh>
       </group>
